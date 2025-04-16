c913f0ef4df6d2e831f9365fec922ccb890f46d1
Agnese Andriani <agnese.andriani@edu.unito.it>
2025-04-16 16:27:45 +0200

    commenti home

--- my-app/app/(tabs)/home.tsx
+++ my-app/app/(tabs)/home.tsx
@@ -74,7 +74,7 @@ const styles = StyleSheet.create({
   fullscreen: {
     flex: 1,
   position: 'relative',
-  width: '100%', // ✅ importante: no Dimensions qui
+  width: '100%', 
   height: '100%',
   },
   iframe: {
